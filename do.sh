ddabce6de69fb6ae70846d5856c463dcf055e143
ubuntu_ingo <ubuntu@ingo>
2022-11-23 14:53:06 +0000

    fix script

--- do.sh
+++ do.sh
@@ -6,14 +6,8 @@ export TEST_NPOW=12
 cargo run 
 cargo run --release
 
-cd ../rust-rw-device-G2
+cd ../rust-rw-device-g2
 export TEST_NPOW=15
 #bn254
 cargo run --release
-
-cd ../rust-bench;
-#bn254
-export TEST_NPOW=12; cargo test --release
-export TEST_NPOW=3; cargo test --release
-export TEST_NPOW=0; cargo test --release
  
